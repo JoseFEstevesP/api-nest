b980fd6dd5f44f984aecb86c8afa31f16eda8b2d
José Esteves <joseesteves199930@gmail.com>
2025-07-31 13:37:16 -0400

    feat: actualizar configuración de pnpm y mejorar la estructura de metadata

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,27 +1,21 @@
-import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  test: {
-    include: ['src/**/*.spec.ts'],
-    exclude: ['node_modules', 'dist'],
-    globals: true,
-    environment: 'node',
-    setupFiles: ['src/test/setup.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      exclude: ['node_modules', 'test', 'src/main.ts'],
-    },
-    alias: {
-      '@': './src',
-    },
-    deps: {
-      register: ['@swc/core'],
-    },
-    transformMode: {
-      web: [/.[jt]sx$/],
-    },
-  },
+	plugins: [tsconfigPaths()],
+	test: {
+		include: ['src/**/*.spec.ts'],
+		exclude: ['node_modules', 'dist'],
+		globals: true,
+		environment: 'node',
+		setupFiles: ['src/test/setup.ts'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'lcov'],
+			exclude: ['node_modules', 'test', 'src/main.ts'],
+		},
+		alias: {
+			'@': './src',
+		},
+	},
 });
